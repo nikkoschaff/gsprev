0b7a19cb483cbf5288aa08e842c6deaf00d2bc81
Nikko Schaff <nikko.schaff@gmail.com>
2012-02-24 20:00:43 -0500

    saving new notes - Turns out problem all stems from bad getIDfromValue method, which returns too many possibilities and there's no way of finding out which is the right one at first glance.

--- Notes.h
+++ Notes.h
@@ -8,6 +8,25 @@ $$$1: TODO reflect changes:
 $$$3: Set standards
 
 I: TODO
+
+
+1: The ID-getter only goes to the first possible one.  Other problems show up	
+	like blanks table spaces, but that's because only the first IDNUM in the full
+	possible set gets returned.  This should either be changed or at least understood
+	throughout the program
+2: The Model, when working with new inputs within the system, MUST, I REPEAT, MUST know
+	everything about the associations as they need to know to extrapolate.  Although
+	having the user select from a displayed list the options that reflect db entries,
+	and letting that list send through to the model is easy to work with and doesn't
+	involve searching or predicting, any action within the model that doesn't have user
+	input *******MUST******* already know the IDs, or else it will completely crash and burn
+	when it tries to predict them.
+			->getIDfromValue *may* possibly be a useless or unpredictable function
+3:	Grades don't seem to be accurate when setting in DB.  See if it's a translation problem
+	or if they're not being graded properly from the get-go
+
+
+
 	A: Better ID-maker in GS
 	C: Finish GSM printresults()
 
