f123baf97a77f9676468184898dea1f71654652b
Nikko Schaff <nikko.schaff@gmail.com>
2012-02-29 01:40:44 -0500

    Cleaned up some files, improved runtime speed and reduced disk space requirements by clearing out unneeded libraries

--- GradeSnapMain.h
+++ GradeSnapMain.h
@@ -1,22 +1,39 @@
 
-
 #ifndef GRADESNAPMAIN_H_
 #define GRADESNAPMAIN_H_
+
+#include <wx/wx.h>
 
 #include "GradeSnapModel.h"
 #include "DBManager.h"
 
-class BasicApplication : public wxApp
+class MyApp: public wxApp
 {
-public:
-	virtual bool OnInit();
+    virtual bool OnInit();
 };
+
  
-class BasicFrame : public wxFrame
+class MainFrame: public wxFrame
 {
 public:
-	BasicFrame( const wxChar *title, int xpos, int ypos, int width, int height );
-	~BasicFrame();
+ 
+    MainFrame(const wxString& title, 
+           const wxPoint& pos, const wxSize& size);
+
+    void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+ 
+private:
+
+    DECLARE_EVENT_TABLE()
+
+};
+
+enum
+{
+    ID_Quit = 1,
+    ID_About,
+
 };
 
 #endif
