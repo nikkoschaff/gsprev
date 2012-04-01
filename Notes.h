77fbd848cd26a37c4d06aa8aa75aff2e75a07ca6
Nikko Schaff <nikko.schaff@gmail.com>
2012-03-31 23:14:06 -0400

    "done"

--- Notes.h
+++ Notes.h
@@ -1,31 +1,5 @@
 /*
 
-$$$1: TODO reflect changes: 
-	A: Teacher.h to match DD
-	B: only answers, no questions for now (reflect in DD)
-	C: reflect changes in DD (q/a now in vector, not map)
-	D: reflect changes in DD (no private EvalHelper in class)
-$$$3: Set standards
-
-I: TODO
-	A: Viewer
-***		1: TODO show exam image when doing refreshInfo
-			-> Yeah....get around to that
-$$$		4: Create a warning if no key selected
-
-
-	B: GUI - other issues
-$$$		1: Image bitmaps aren't loaded correctly upon startup
-
-
-II: Database concerns
-	1: What happens if searching out of index or availability? (or at a table that doesn't exist)
-		-> BREAKS if asking for something that doesn't exist
-	2: It's easy to have linked duplicates.  Would that also unnecessarily duplicate searches?
-	
-
-III: Image Evaluator -> modify to work with new image, and apply needed changes
-		(more to be added as needed)
 
 
 
